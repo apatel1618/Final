551ee4360728690d296afe8b0025ec07649f9086
apatel1618 <96014930+apatel1618@users.noreply.github.com>
2022-12-05 22:04:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('mainindex.html')
+    return render_template('templates/mainindex.html')
 
 
 # @app.route('/login', methods=['GET', 'POST'])
