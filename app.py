d9661a2b53d4c5223bc026bec81a90ca84810fd6
apatel1618 <96014930+apatel1618@users.noreply.github.com>
2022-12-05 21:41:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import email
 from sqlite3 import Time
 from telnetlib import NOP
 from time import time
-from flask import flask , render_template, request, redirect, url_for, session
+from flask import Flask , render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
